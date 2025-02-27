04bade4d7c21b627e9c4365a42d62b6170c74caf
roxalopes <vlopes.ti@gmail.com>
2025-02-27 18:38:44 -0300

    Distribuidora.Estado done. Correção de formatação de moeda.

--- ConsoleApp.Distribuidora/Program.cs
+++ ConsoleApp.Distribuidora/Program.cs
@@ -2,6 +2,7 @@
 // Obtém o caminho do JSON no diretório do executável
 using ConsoleApp.Distribuidora.Entity;
 using Newtonsoft.Json;
+using System.Globalization;
 
 string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DadosFaturamento", "dados.json");
 
@@ -11,10 +12,24 @@ if (!File.Exists(filePath))
     return;
 }
 
-// Lê o conteúdo do JSON
 string json = File.ReadAllText(filePath);
 
-// Desserializa os dados para uma lista de objetos
 List<FaturamentoData> faturamentoMensal = JsonConvert.DeserializeObject<List<FaturamentoData>>(json);
 
-Console.WriteLine("Arquivo JSON carregado com sucesso!");
+// Filtrar apenas os dias com faturamento (excluir valores 0.0)
+var diasComFaturamento = faturamentoMensal.Where(d => d.Valor > 0).ToList();
+
+// Encontrar o menor e o maior faturamento
+double menorFaturamento = diasComFaturamento.Min(d => d.Valor);
+double maiorFaturamento = diasComFaturamento.Max(d => d.Valor);
+
+// Calcular a média mensal considerando apenas os dias com faturamento
+double mediaMensal = diasComFaturamento.Average(d => d.Valor);
+
+// Contar quantos dias tiveram faturamento superior à média
+int diasAcimaDaMedia = diasComFaturamento.Count(d => d.Valor > mediaMensal);
+
+// Exibir os resultados
+Console.WriteLine($"Menor faturamento do mês: {menorFaturamento.ToString("C", new CultureInfo("pt-BR"))}");
+Console.WriteLine($"Maior faturamento do mês: {maiorFaturamento.ToString("C", new CultureInfo("pt-BR"))}");
+Console.WriteLine($"Número de dias com faturamento acima da média: {diasAcimaDaMedia}");
---
+++ ConsoleApp.DistribuidoraEstado/Program.cs
@@ -0,0 +1,27 @@
+﻿//4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+//• SP – R$67.836,43
+//• RJ – R$36.678,66
+//• MG – R$29.229,88
+//• ES – R$27.165,48
+//• Outros – R$19.849,53
+//Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal
+//da distribuidora. 
+
+using System.Globalization;
+
+var listFaturamentos = new Dictionary<string, double>();
+listFaturamentos.Add("SP", 67836.43);
+listFaturamentos.Add("RJ", 36678.66);
+listFaturamentos.Add("MG", 29229.88);
+listFaturamentos.Add("ES", 27165.48);
+listFaturamentos.Add("Outros", 19849.53);
+
+double totalMensal = listFaturamentos.Select(s => s.Value).Sum();
+Console.WriteLine($"Faturamento do mês: {totalMensal.ToString("C", new CultureInfo("pt-BR"))}");
+
+foreach(var item in listFaturamentos)
+{
+    Console.WriteLine($"Estado: {item.Key}, Faturamento: {item.Value.ToString("C", new CultureInfo("pt-BR"))}, Percentual: {(item.Value/totalMensal)*100:F2}%");
+}
+
+
